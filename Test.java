0b1d51e1b155af993365e3de9bceecb9a9c2f382
SebianNovaroAritonang <125192699+bartanarchy@users.noreply.github.com>
2025-09-30 20:03:26 +0700

    Delete Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hi, ini baris kode pertama saya");
-        System.out.println("hi there");
-        System.out.println("hello there");
-        System.out.println("other words....");
-}
-}
